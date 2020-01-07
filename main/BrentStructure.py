12781e2b55d4c59318a390fcf950221308eb7028
BvDod <43404889+BvDod@users.noreply.github.com>
2020-01-07 11:05:45 +0100

    added rollback fuctionality if no legal moves left

--- main/BrentStructure.py
+++ main/BrentStructure.py
@@ -8,6 +8,9 @@ class Amino(object):
         self.atype = atype
         self.fold = fold
         self.coordinates = coordinates
+        
+        # Contains illegal folds for this amino based on that the next location wouldnt have legal moves.
+        self.illegal_folds = []
     
     def __str__(self):
         return str((self.atype, self.fold, self.coordinates))
@@ -47,24 +50,35 @@ class Protein(object):
         # Adds the first amino to the chain, direction is hard-coded as "up".
         self.chain.append(Amino(amino_string[0], 2, [0,0]))
     	
-        # Skips the first char and the last char with the index.
-        for char in amino_string[1:-1]:
-
+        self.char_counter = 1
+        # Skips the first char the index.
+        while self.char_counter < len(amino_string):
+            print(str(self.char_counter))
+            char = amino_string[self.char_counter]
             # Get the location the last amino folded to.
             # Note: an index of -1 gets the last object in a list.
             amino_xy = self.chain[-1].get_fold_coordinates()
 
             
-            # Determines which fold to pick.
-            fold = fold_selector(amino_xy, char, self.chain)
+            # Last amino always has fold of 0.
+            if self.char_counter + 1 == len(amino_string):
+                fold = 0
+            
+            # Determine which fold to pick
+            else:
+                illegal_folds = None
+                fold = fold_selector(amino_xy, char, self.chain, illegal_folds)
 
-            # Adds amino to the protein chain.
+                 # If no legal moves are available, the last move needs to be reversed.
+                if not fold:
+                    self.redo_last_fold()
+                    continue
+            
+            # Adds amino to the protein chain. 
             self.chain.append(Amino(char, fold, amino_xy))
+            self.char_counter += 1
+           
         
-        # The last amino needs a fold of "0"
-        amino_xy = self.chain[-1].get_fold_coordinates()
-        fold = 0
-        self.chain.append(Amino(char, fold, amino_xy))
 
     # Outputs a list like the excercise requires
     def get_output_list(self):
@@ -113,7 +127,7 @@ class Protein(object):
             print( "       " + str(x) + "       ", end="")
         print("")
 
-        for row in range(y_range[1] - y_range[0] + 1):
+        for row in range(0, y_range[1] - y_range[0] + 1):
             
             # Print the y coordinates
             print( str(row), end="")
@@ -136,15 +150,48 @@ class Protein(object):
         #TODO
         pass
 
+    def redo_last_fold(self):
+        print("redo")
+        # Store the illegal fold in the amino class.
+        last_amino = self.chain[-1]
+        last_amino.illegal_folds.append(last_amino.fold)
+
+        print(last_amino.fold)
+
+        # Get new move with illegal moves excluded.
+        fold = fold_selector(last_amino.coordinates, last_amino.atype, self.chain[:-1], last_amino.illegal_folds)
+        
+        # Replace the previous illegal fold with a new fold
+        last_amino.fold = fold
+        print(fold)
+        print("test34")
+
+
+        # If still no fold found, also redo move before that. Char loop in init needs to go back 1 step.
+        if not fold:
+            self.chain.remove(last_amino)
+            self.char_counter -= 1
+            self.redo_last_fold()
+                
+
+
 # The actual algo for selecting the fold the chain will make.
-def fold_selector(xy, char, chain):
+def fold_selector(xy, char, chain, illegal_moves):
     
     legal_moves = get_legal_moves(xy, chain)
     
-    # Selects a random move
-    selected_move = random.choice(legal_moves)
+    # Remove illegal moves from legal moves list.
+    if illegal_moves:  
+        for move in illegal_moves:
+            if move in legal_moves:
+                legal_moves.remove(move)
+            
+    # Selects a random move if at least 1 legal moves exists
+    if legal_moves:
+        return random.choice(legal_moves)
 
-    return selected_move
+    # If no legal moves exist, return False
+    return False
     
 
     # Finds all the legal moves that can be made from the current position.
@@ -172,13 +219,9 @@ def get_legal_moves(xy, chain):
     for moves in moves_xydelta:
         legal_moves.append(moves[0])
 
-    # if no legal moves
-    if not legal_moves:  
-        raise Exception ("No legal moves remaings, still needs to be implemented")
-
     return legal_moves
 
 if __name__ == "__main__":
-    protein1 = Protein("HPHPHPHHHPP")
+    protein1 = Protein("HPHPHPHHHPPHPPHPHPHPHHPPHP")
     protein1.get_output_list()
     protein1.print_map()
