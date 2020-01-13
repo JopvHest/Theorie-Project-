8a5e04934fcf3ff375b8c04f1e5115ad224fe693
Jop van Hest <jop@Jops-MacBook-Pro.local>
2020-01-13 13:46:38 +0100

    Added new visualisation for 2d chains

--- main/classes/protein.py
+++ main/classes/protein.py
@@ -4,6 +4,9 @@ from classes.amino import Amino
 
 import string
 
+import matplotlib.pyplot as plt
+
+
 # Represents a chain of amino acids and orders them.
 class Protein(object):
 
@@ -11,20 +14,20 @@ class Protein(object):
 
         if mode == "2d" or mode == "2D":
             self.mode_3d = False
-        
+
         elif mode == "3d" or mode == "3D":
             self.mode_3d = True
-        
+
         else:
             raise Exception("Dimension mode not valid")
 
         # The list which contains the ordered and connected aminos.
         self.chain = []
 
-        
+
         # The string of the protein, make it case insensitive
         self.amino_string = amino_string.upper()
-        
+
         # Check if amino string contains chars other than H, C and P
         illegal_chars = list(string.ascii_uppercase)
         illegal_chars.remove("H")
@@ -50,12 +53,12 @@ class Protein(object):
 
     # Prints the matrix of the protein.
     def print_map(self):
-        
+
         if self.mode_3d == False:
             matrix = self.matrix
             # Print matrix using pandas
             print(DataFrame(matrix))
-        
+
         if self.mode_3d == True:
             matrix = self.matrix
 
@@ -63,8 +66,30 @@ class Protein(object):
             for layer in matrix:
                 # Print matrix using pandas
                 print(DataFrame(layer))
-        
 
+    def print_protein(self):
+        if self.mode_3d == False:
+            x_points, y_points, colors = [], [], []
+            for amino in self.chain:
+                x_points.append(amino.coordinates[0])
+                y_points.append(amino.coordinates[1])
+
+                if amino.atype == 'H':
+                    colors.append((1,0,0))
+                elif amino.atype == 'P':
+                    colors.append((0,0,1))
+
+            fig, ax = plt.subplots()
+            plt.scatter(x_points, y_points, c = colors, s = 200)
+            plt.plot(x_points, y_points, linestyle='-', color='0.4')
+            ax.xaxis.set_major_locator(plt.MultipleLocator(1))
+            ax.yaxis.set_major_locator(plt.MultipleLocator(1))
+            plt.grid()
+
+            plt.show()
+
+        if self.mode_3d == True:
+            print("3D version not yet implemented. ")
 
     # Outputs a list like the excercise requires
     def get_output_list(self):
@@ -73,6 +98,6 @@ class Protein(object):
         for amino in self.chain:
             print(str(amino.get_amino_output()))
         print("")
-    
+
     def get_score(self):
         return get_score(self.chain)
--- main/main.py
+++ main/main.py
@@ -6,13 +6,8 @@ from algorithms.DepthSearch import depth_search
 
 if __name__ == "__main__":
 
-    protein1 = Protein("HPPH")
-    random_search(protein1)
+    protein1 = Protein("HPPHPPPHPH", "2D")
+    depth_search(protein1)
 
     protein1.print_map()
-
-    start_time = time.clock()
-    newchain = copy.deepcopy(chain)
-    amino1 = Amino("H", -2, [3,5])
-    newchain.append(amino1)
-    print("--- %s seconds ---" % (time.clock() - start_time))
+    protein1.print_protein()
