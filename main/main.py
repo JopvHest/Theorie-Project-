6bcd50b699e38d792781e9882a1e367a75151f7b
Luitzen <kleinehector@gmail.com>
2020-01-14 17:38:13 +0100

    breadth search works with deepcopy, commented breadth search

--- main/main.py
+++ main/main.py
@@ -6,16 +6,16 @@ from algorithms.RandomSearch import random_search
 from algorithms.DepthSearch import depth_search
 from algorithms.DepthSearchLookahead import depth_search_lookahead
 from algorithms.helpers import get_fold_number
-from algorithms.breadth_first import breadth_search
+from algorithms.BreadthSearch import breadth_search
 
 
 if __name__ == "__main__":
 
-    protein1 = Protein("HHPPPPPPPH", "2D")
+    protein1 = Protein("HHPPPPP", "2D")
     breadth_search(protein1)
 
-    protein1 = Protein("HPPCPPCHHHHPHHPPP", "2D")
-    depth_search_lookahead(protein1, 10)
+    # protein1 = Protein("HPPCPPCHHHHPHHPPP", "2D")
+    # depth_search_lookahead(protein1, 10)
 
-    protein1.print_map()
-    protein1.print_protein()
+    # protein1.print_map()
+    # protein1.print_protein()
