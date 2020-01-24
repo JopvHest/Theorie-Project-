a95802b857627a348958d7107cb786bd9b3cfdde
Luitzen <kleinehector@gmail.com>
2020-01-24 11:06:50 +0100

    made new iterative algo from hillclimber

--- main/main.py
+++ main/main.py
@@ -9,11 +9,12 @@ from algorithms.BreadthSearch import breadth_search
 from algorithms.BeamSearch import beam_search
 from algorithms.DepthSearchIterative import depth_search_iterative
 from algorithms.DepthSearchFoldSpots import depth_search_iterative_and_spots
+from algorithms.IterativeAlgorithm2 import hill_climbing
 from algorithms.IterativeAlgorithm import iterative_algorithm
 from algorithms.BranchAndBoundLookahead import branch_and_bound_lookahead
 from algorithms.BranchAndBoundRandom import branch_and_bound_random
-from algorithms.HillClimbing import hill_climbing
-from algorithms.HillClimbingAnnealing import hill_climbing_annealing
+# from algorithms.HillClimbing import hill_climbing
+# cfrom algorithms.HillClimbingAnnealing import hill_climbing_annealing
 
 
 from functions.GetMatrix import get_matrix_efficient
@@ -22,13 +23,13 @@ from functions.GetScore import get_score_efficient
 
 if __name__ == "__main__":
 
-    chain = [Amino("H", 2, [0, 0]), Amino("H", 2, [0, 1]), Amino("H", 1, [0, 2]), Amino("P", -2, [1, 2]), Amino('H', -2, [1, 1]), Amino("H", 1, [1, 0]), Amino("P", 2, [2, 0]), Amino("H", 0, [2, 1])]
+    # chain = [Amino("0", 1, [0, 0]), Amino("1", 1, [1, 0]), Amino("2", 1, [2, 0]), Amino("3", 1, [3, 0]), Amino('4', 1, [4, 0]), Amino("5", 1, [5, 0]), Amino("6", 1, [6, 0]), Amino("7", 2, [6, 1])]
     # start_time = time.clock()
     
     protein1 = Protein("HHHPHHPH", "2D")
-    protein1.chain.chain_list = chain
-    for amino in protein1.chain.chain_list:
-        print(amino)
+    # protein1.chain.chain_list = chain
+    # for amino in protein1.chain.chain_list:
+    #     print(amino)
     # protein
 
     # random_search(protein1)
@@ -36,30 +37,27 @@ if __name__ == "__main__":
     # print("--- %s seconds ---" % (time.clock() - start_time))
     # protein1.print_protein()
     
-<<<<<<< HEAD
 
-    pato, chain2 = iterative_algorithm(protein1.chain, 1)
-    protein1.chain.chain_list = pato
-=======
-    
-#     protein1 = Protein('PPCHHPPCHPPPPCHHHHCHHPPHHPPPPHHPPHPP', "3D")
 
-#     hill_climbing_annealing(protein1, 100000)                                           
-#     #branch_and_bound_random(protein1, 1, 0, 0.6, 0.3)
-#     #depth_search_lookahead(protein1, 10, 1)
-    
-#     print(protein1.get_score())
-#     protein1.print_protein()
-#     print("--- %s seconds ---" % (time.clock() - start_time))
-# >>>>>>> 1594a7e2f751ef02eba4276acac08202e92c488b
+    # pato, chain2 = iterative_algorithm(protein1.chain, 1)
+    # protein1.chain.chain_list = pato
+
+    hill_climbing(protein1, 10)
     
     for amino in protein1.chain.chain_list:
         print(amino)
+    
+    
+    
     # protein1.print_protein()
-    protein1.chain.chain_list = chain2
+    
+    
+    
+    # # protein1.print_protein()
+    # protein1.chain.chain_list = chain2
 
-    for amino in protein1.chain.chain_list:
-        print(amino)
+    # for amino in protein1.chain.chain_list:
+        # print(amino)
 
     # matrix, offset = get_matrix_efficient(chain)
 
